7676307f7b05b8a44e4544e0e8e9dfba9cff8dac
Carloz <112228701+Carlozzzzz@users.noreply.github.com>
2023-04-06 11:30:11 +0800

    remove some list

--- CI4_TodoList/app/Views/pages/todolistfile.php
+++ CI4_TodoList/app/Views/pages/todolistfile.php
@@ -60,13 +60,6 @@
                 </div>
             </div>
         </div>
-
-        <div class="container">
-            <div class="item" draggable="true">Item 1</div>
-            <div class="item" draggable="true">Item 2</div>
-            <div class="item" draggable="true">Item 3</div>
-            <div class="item" draggable="true">Item 4</div>
-        </div>
     </section>
 
     <script type="text/javascript">
@@ -90,55 +83,5 @@
             });
         }
 
-        jQuery(document).ready({
-            const items = document.querySelectorAll('.item');
-            let draggedItem = null;
-
-            // Add event listeners for each item
-            items.forEach(item => {
-            item.addEventListener('dragstart', function() {
-                draggedItem = this;
-                setTimeout(() => {
-                this.style.display = 'none';
-                }, 0);
-            });
-
-            item.addEventListener('dragend', function() {
-                setTimeout(() => {
-                draggedItem.style.display = 'block';
-                draggedItem = null;
-                }, 0);
-            });
-            });
-
-            // Add event listeners for container
-            const container = document.querySelector('.container');
-
-            container.addEventListener('dragover', function(e) {
-            e.preventDefault();
-            const afterElement = getDragAfterElement(container, e.clientY);
-            const draggable = document.querySelector('.dragging');
-            if (afterElement == null) {
-                container.appendChild(draggable);
-            } else {
-                container.insertBefore(draggable, afterElement);
-            }
-            });
-
-            // Function to get element being dragged after the element it is dropped on
-            function getDragAfterElement(container, y) {
-            const draggableElements = [...container.querySelectorAll('.item:not(.dragging)')];
-            return draggableElements.reduce((closest, child) => {
-                const box = child.getBoundingClientRect();
-                const offset = y - box.top - box.height / 2;
-                if (offset < 0 && offset > closest.offset) {
-                return { offset: offset, element: child };
-                } else {
-                return closest;
-                }
-            }, { offset: Number.NEGATIVE_INFINITY }).element;
-            }
-
-        });
     </script>
 <?= $this->endSection(); ?>
